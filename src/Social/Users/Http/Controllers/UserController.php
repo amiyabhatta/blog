49bdba29294921814165d4c15269683543e1d731
System Administrator <root@BG-NOI-0198.local>
2018-01-04 16:50:02 +0530

    3rd change

--- src/Social/Users/Http/Controllers/UserController.php
+++ src/Social/Users/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     }
      
     public function getUser(){
-        return $this->usercontainer->getUsersss123();
+        return $this->usercontainer->getUsersss90();
     } 
     
 
