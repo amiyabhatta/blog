3b61ca71ee9010d6b2220a6b8fe58a3ec5aa70a7
amiyabhatta <amiyabhatta007@gmail.com>
2018-01-04 16:03:17 +0530

    Update UserController.php

--- src/Social/Users/Http/Controllers/UserController.php
+++ src/Social/Users/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     }
      
     public function getUser(){
-        return $this->usercontainer->getUser();
+        return $this->usercontainer->getUsersss();
     } 
   
 }
